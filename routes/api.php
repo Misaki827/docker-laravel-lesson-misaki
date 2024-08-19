c70bd2cb2c61ccf470447b560f36e7f75ea7a3f6
Misaki827 <sota.misaki@relic.co.jp>
2024-08-19 13:36:46 +0900

    コミット

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::get('/health', function () {
     return response()->json([
         'message' => 'API is healthy'
     ]);
 });
+
+Route::apiResource('todos', 'App\Http\Controllers\TodoController');
